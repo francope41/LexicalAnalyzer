b78255f44ed4cdd477ee70abef5d486d07618e26
btz670 <btz670@fox01.cs.utsarr.net>
2023-02-13 17:47:32 -0600

    main

--- workdir/exec.sh
+++ workdir/exec.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-python ../main.py $1
+if [ $# -eq 0 ]
+  then
+    echo "Path to file required to run (EXAMPLE: \"./exec.sh ../samples/badbool.frag\")"
+else
+    python ../main.py $1
+fi
